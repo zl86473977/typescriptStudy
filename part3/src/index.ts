580b976af30500c652fb6e12eae7b8d0b256a6b2
leozhang <871002990@qq.com>
2024-02-06 16:50:28 +0800

    ts学习(12/30)webpack打包ts代码

--- part3/src/index.ts
+++ part3/src/index.ts
@@ -9,6 +9,12 @@ console.log("haha");
 console.log("hi:", hi);
 
 const obj = { name: "孙悟空", age: 111 };
+obj.age = 18;
 console.log(obj);
-obj.age = 18
-console.log(obj);
+
+let fn = (a: number, b: number) => a + b;
+fn(123, 456);
+
+// Promise.resolve(1).then(res => {
+//   console.log(res);
+// })
--- part3/webpack.config.js
+++ part3/webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
     // 指定打包文件的目录
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    // 告诉webpack不适用箭头函数
+    environment: {
+      arrowFunction: false
+    }
   },
   mode: "production",
 
@@ -25,31 +29,32 @@ module.exports = {
         // 要使用的loader
         use: [
           // 配置babel
-        //   {
-        //     // 指定加载器
-        //     loader: "babel-loader",
-        //     // 设置babel
-        //     options: {
-        //       // 设置预定义的环境
-        //       presets: [
-        //         [
-        //           // 指定环境的插件
-        //           "@babel/preset-env",
-        //           // 配置信息
-        //           {
-        //             // 要兼容的目标浏览器
-        //             target: {
-        //               chrome: "88",
-        //             },
-        //             // 指定corejs的版本
-        //             corejs: "3",
-        //             // 使用corejs的方式"usage" 表示按需加载
-        //             useBuiltIns: "usage",
-        //           },
-        //         ],
-        //       ],
-        //     },
-        //   },
+          {
+            // 指定加载器
+            loader: "babel-loader",
+            // 设置babel
+            options: {
+              // 设置预定义的环境
+              presets: [
+                [
+                  // 指定环境的插件
+                  "@babel/preset-env",
+                  // 配置信息
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      chrome: "88",
+                      ie: 11
+                    },
+                    // 指定corejs的版本
+                    corejs: "3",
+                    // 使用corejs的方式"usage" 表示按需加载
+                    useBuiltIns: "usage",
+                  },
+                ],
+              ],
+            },
+          },
           "ts-loader",
         ],
         // 要排除的文件
